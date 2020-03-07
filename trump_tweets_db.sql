ce6491da491b51501013c65c32c212205237ec11
Scot Thompson <satgatech180@gmail.com>
2020-03-07 12:05:25 -0500

    ETL Project SQL Data Tables Rev 1

--- trump_tweets_db.sql
+++ trump_tweets_db.sql
@@ -1,56 +1,56 @@
 CREATE TABLE nasdaq(
-	date DATE NOT NULL PK,
+	date DATE NOT NULL PRIMARY KEY,
 	open FLOAT NOT NULL,
 	high FLOAT NOT NULL,
 	low FLOAT NOT NULL,
 	close FLOAT NOT NULL,
 	adjclose FLOAT NOT NULL,
-	volume INT NOT NULL,
+	volume INT NOT NULL
 );
 
 CREATE TABLE dow(
-	date DATE NOT NULL PK,
+	date DATE NOT NULL PRIMARY KEY,
 	open FLOAT NOT NULL,
 	high FLOAT NOT NULL,
 	low FLOAT NOT NULL,
 	close FLOAT NOT NULL,
 	adjclose FLOAT NOT NULL,
-	volume INT NOT NULL,
+	volume INT NOT NULL
 );
 
 CREATE TABLE s_and_p(
-	date DATE NOT NULL PK,
+	date DATE NOT NULL PRIMARY KEY,
 	open FLOAT NOT NULL,
 	high FLOAT NOT NULL,
 	low FLOAT NOT NULL,
 	close FLOAT NOT NULL,
 	adjclose FLOAT NOT NULL,
-	volume INT NOT NULL,
+	volume INT NOT NULL
 );
 
 CREATE TABLE russell(
-	date DATE NOT NULL PK,
+	date DATE NOT NULL PRIMARY KEY,
 	open FLOAT NOT NULL,
 	high FLOAT NOT NULL,
 	low FLOAT NOT NULL,
 	close FLOAT NOT NULL,
 	adjclose FLOAT NOT NULL,
-	volume INT NOT NULL,
+	volume INT NOT NULL
 );
 
 CREATE TABLE usd_euroO(
-	date DATE NOT NULL PK,
+	date DATE NOT NULL PRIMARY KEY,
 	open FLOAT NOT NULL,
 	high FLOAT NOT NULL,
 	low FLOAT NOT NULL,
 	close FLOAT NOT NULL,
 	adjclose FLOAT NOT NULL,
-	volume INT NOT NULL,
+	volume INT NOT NULL
 );
 
 CREATE TABLE trump_tweets(
 	text TEXT NOT NULL,
-	date DATE NOT NULL,
+	date DATE NOT NULL PRIMARY KEY,
 	retweet_count INT NOT NULL,
 	favorite_count INT NOT NULL,
 	id_str VARCHAR(30) NOT NULL
